85e7d21adb76d28f17e988e06bc4963deeebe611
maxcostatismart <max.costa@tismart.com>
2022-05-25 10:37:44 -0500

    TEST USER

--- src/test/java/upn/proj/sowad/UserTest.java
+++ src/test/java/upn/proj/sowad/UserTest.java
@@ -7,31 +7,41 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import upn.proj.sowad.dto.UserDto;
+import upn.proj.sowad.entities.User;
+import upn.proj.sowad.exception.domain.EmailExistException;
+import upn.proj.sowad.exception.domain.UserNotFoundException;
+import upn.proj.sowad.exception.domain.UsernameExistException;
 import upn.proj.sowad.services.UserService;
+
+import javax.mail.MessagingException;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.List;
+
 import static upn.proj.sowad.constant.UserImplConstant.*;
 
 @SpringBootTest
 public class UserTest {
+
+    public Integer count = 0;
     
     @Autowired
     private UserService userService;
 
     @Test
-    public void registerUserTest1(){
-        UserDto userTest = new UserDto();
-        userTest.setFirstName("admin");
-        userTest.setLastName("admin");
-        userTest.setUsername("admin");
-        userTest.setEmail("admin@admin.com");
-        userTest.setPassword("admin");
-        
+    public void registerUser_whenUsernameIsAlreadyRegistered_recieveException(){
+        String expectedMessage = USERNAME_ALREADY_EXISTS;
+        String realMessage = "";
         try {
-            this.userService.register("Chad", "Mamani", "chadma", "chadma@gmail.com", "admin", null);
-        } catch (Exception e) {
-            String expectedMessage = USERNAME_ALREADY_EXISTS;
-            assertEquals(expectedMessage, e.getMessage());
+            if(this.userService.findUserByUsername("johndoe")!=null)
+                this.userService.deleteUser("johndoe");
+            this.userService.register("John", "Doe", "johndoe", "johndoe@gmail.com", "johndoe", null);
+            this.userService.register("John", "Doe", "johndoe", "johndoe@gmail.com", "johndoe", null);
+        } catch (UsernameExistException | UserNotFoundException | EmailExistException | MessagingException | IOException e) {
+            realMessage = e.getMessage();
         }
 
+        assertEquals(expectedMessage,realMessage);
     }
 
     
