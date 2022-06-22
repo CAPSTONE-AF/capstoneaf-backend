41067c548c278e5c5739911bd78b0117ee43dc2b
maxcostatismart <max.costa@tismart.com>
2022-06-22 00:57:07 -0500

    ALL UNIT TEST IMPLEMENTED!

--- src/test/java/upn/proj/sowad/QuizTest.java
+++ src/test/java/upn/proj/sowad/QuizTest.java
@@ -3,6 +3,7 @@ package upn.proj.sowad;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import upn.proj.sowad.constant.QuizConstant;
 import upn.proj.sowad.dto.QuizDto;
 import upn.proj.sowad.entities.Tema;
 import upn.proj.sowad.exception.domain.*;
@@ -47,33 +48,111 @@ public class QuizTest {
         }
         assertEquals(expectedMessage, realMessage);
     }
-        @Test
-        public void registerExamen_whenTitleDoesNotExist() throws UtilityException, CursoExistsException, CursoNotFoundException, TemaNotFoundException, TemaExistsException, IOException, NotAnImageFileException {
-            String nombreCursoTest = "unknownCurso";
-            String nombreTemaTest = "unknownTema";
+
+    @Test
+    public void registerExamen_whenTitleDoesNotExist() {
+        String nombreCursoTest = "unknownCurso";
+        String nombreTemaTest = "unknownTema";
+
+        String expectedMessage = QuizConstant.NOT_TITLE_FOUND;
+        String realMessage = "";
+        try {
+
             if (this.cursoService.findCursoByNombre(nombreCursoTest) == null)
                 this.cursoService.addNewCurso(nombreCursoTest, "1");
             if (this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest) == null)
                 this.temaService.addNewTema(nombreCursoTest, nombreTemaTest, "", "1");
+
             QuizDto quizDto = new QuizDto();
             quizDto.setTitle("");
-            quizDto.setDescription("examen1");
+            quizDto.setDescription("examenNuevo");
             quizDto.setIdTema(this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest).getIdTema().toString());
 
-            //String expectedMessage = CURSO_HAS_NO_TITLE;
-           //String realMessage = "";
-            //try {
-              //  QuizDto quizDto = new QuizDto();
-                //quizDto.setTitle("");
-                //this.quizService.registerNewQuiz(quizDto);
+            this.quizService.registerNewQuiz(quizDto);
 
-            //} catch ( UtilityException e) {
-              //  realMessage = e.getMessage();
-           // }
-          //  assertEquals(expectedMessage, realMessage);
-
+        } catch (UtilityException | CursoNotFoundException | CursoExistsException | TemaNotFoundException | IOException | NotAnImageFileException | TemaExistsException e) {
+            realMessage = e.getMessage();
         }
+
+        try {
+            this.cursoService.deleteCurso(nombreCursoTest);
+        } catch (UtilityException e) {
+            e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+
     }
+
+    @Test
+    public void registerExamen_whenHasTemaInvalido() {
+
+        String expectedMessage = TEMA_NOT_FOUND;
+        String realMessage = "";
+        try {
+
+            this.quizService.getAllByTema("999999999");
+
+        } catch (UtilityException e) {
+            realMessage = e.getMessage();
+        }
+        assertEquals(expectedMessage, realMessage);
+
+    }
+
+    @Test
+    public void updateExamen_whenHasIDInvalido() {
+        String nombreCursoTest = "unknownCurso";
+        String nombreTemaTest = "unknownTema";
+        String expectedMessage = QuizConstant.QUIZ_NOT_FOUND;
+        String realMessage = "";
+        try {
+
+            if (this.cursoService.findCursoByNombre(nombreCursoTest) == null)
+                this.cursoService.addNewCurso(nombreCursoTest, "1");
+            if (this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest) == null)
+                this.temaService.addNewTema(nombreCursoTest, nombreTemaTest, "", "1");
+
+            QuizDto quizDto = new QuizDto();
+            quizDto.setTitle("segundo");
+            quizDto.setDescription("examen5");
+            quizDto.setIdTema(this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest).getIdTema().toString());
+            quizDto.setIdQuiz("999999999");
+
+            quizService.updateQuiz(quizDto);
+
+        } catch (UtilityException | CursoNotFoundException | CursoExistsException | TemaNotFoundException | IOException | NotAnImageFileException | TemaExistsException e) {
+            realMessage = e.getMessage();
+        }
+
+        try {
+            this.cursoService.deleteCurso(nombreCursoTest);
+        } catch (UtilityException e) {
+            e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+
+    }
+
+    @Test
+    public void deleteExamen_whenHasIDInvalido() {
+
+        String expectedMessage = QuizConstant.QUIZ_NOT_FOUND;
+        String realMessage = "";
+        try {
+
+
+            quizService.deleteQuiz("999999999");
+
+        } catch (UtilityException e) {
+            realMessage = e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+
+    }
+}
 
 
 
