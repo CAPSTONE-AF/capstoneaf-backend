6dcd47124a27e42f282548756caf348191575961
maxcostatismart <max.costa@tismart.com>
2022-06-21 23:01:10 -0500

    UNIT TESTS PT7

--- src/test/java/upn/proj/sowad/QuestionTest.java
+++ src/test/java/upn/proj/sowad/QuestionTest.java
@@ -102,5 +102,170 @@ public class QuestionTest {
         assertEquals(expectedMessage, realMessage);
     }
 
+    @Test
+    public void registerQuestion_whenHasEmptyOptions() {
+        String nombreCursoTest = "unknownCurso";
+        String nombreTemaTest = "unknownTema";
+        String expectedMessage = QuestionConstant.NOT_OPTION1_FOUND;
+        String realMessage = "";
+        try {
+
+            if (this.cursoService.findCursoByNombre(nombreCursoTest) == null)
+                this.cursoService.addNewCurso(nombreCursoTest, "1");
+
+            if (this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest) == null)
+                this.temaService.addNewTema(nombreCursoTest, nombreTemaTest, "", "1");
+
+            QuizDto quizDto = new QuizDto();
+            quizDto.setTitle("nuevo");
+            quizDto.setDescription("examen1");
+            quizDto.setIdTema(this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest).getIdTema().toString()); //este idTema no existe
+
+
+            QuestionDto questionDto = new QuestionDto();
+            questionDto.setIdQuiz(this.quizService.registerNewQuiz(quizDto).getIdQuiz());
+            questionDto.setContent("¿Cuánto es 2 - 2?");
+            questionDto.setOption1("");
+            questionDto.setOption2("3");
+            questionDto.setOption3("0");
+            questionDto.setOption4("1");
+            questionDto.setAnswer("OP3");
+
+            this.questionService.registerQuestion(questionDto);
+        } catch (UtilityException | CursoNotFoundException | CursoExistsException | TemaNotFoundException | IOException | NotAnImageFileException | TemaExistsException e) {
+            realMessage = e.getMessage();
+        }
+
+        try {
+            this.cursoService.deleteCurso(nombreCursoTest);
+        } catch (UtilityException e) {
+            e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+    }
+
+    @Test
+    public void registerQuestion_whenHasEmptyAnswer() {
+        String nombreCursoTest = "unknownCurso";
+        String nombreTemaTest = "unknownTema";
+        String expectedMessage = QuestionConstant.NOT_ANSWER_FOUND;
+        String realMessage = "";
+        try {
+
+            if (this.cursoService.findCursoByNombre(nombreCursoTest) == null)
+                this.cursoService.addNewCurso(nombreCursoTest, "1");
+
+            if (this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest) == null)
+                this.temaService.addNewTema(nombreCursoTest, nombreTemaTest, "", "1");
+
+            QuizDto quizDto = new QuizDto();
+            quizDto.setTitle("nuevo");
+            quizDto.setDescription("examen1");
+            quizDto.setIdTema(this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest).getIdTema().toString()); //este idTema no existe
+
+
+            QuestionDto questionDto = new QuestionDto();
+            questionDto.setIdQuiz(this.quizService.registerNewQuiz(quizDto).getIdQuiz());
+            questionDto.setContent("¿Cuánto es 2 - 2?");
+            questionDto.setOption1("1");
+            questionDto.setOption2("3");
+            questionDto.setOption3("0");
+            questionDto.setOption4("1");
+            questionDto.setAnswer("");
+
+            this.questionService.registerQuestion(questionDto);
+        } catch (UtilityException | CursoNotFoundException | CursoExistsException | TemaNotFoundException | IOException | NotAnImageFileException | TemaExistsException e) {
+            realMessage = e.getMessage();
+        }
+
+        try {
+            this.cursoService.deleteCurso(nombreCursoTest);
+        } catch (UtilityException e) {
+            e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+    }
+
+    @Test
+    public void registerQuestion_whenHasInvalidQuiz() {
+        String expectedMessage = QuizConstant.QUIZ_NOT_FOUND;
+        String realMessage = "";
+        try {
+
+            QuestionDto questionDto = new QuestionDto();
+            questionDto.setIdQuiz("999999999");
+            questionDto.setContent("¿Cuánto es 2 - 2?");
+            questionDto.setOption1("1");
+            questionDto.setOption2("3");
+            questionDto.setOption3("0");
+            questionDto.setOption4("1");
+            questionDto.setAnswer("");
+
+            this.questionService.registerQuestion(questionDto);
+        } catch (UtilityException e) {
+            realMessage = e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+    }
+
+    @Test
+    public void getAllQuestions_whenPassInvalidQuizId() {
+        String expectedMessage = QuizConstant.QUIZ_NOT_FOUND;
+        String realMessage = "";
+        try {
+            this.questionService.getAllByQuiz("999999999");
+        } catch (UtilityException e) {
+            realMessage = e.getMessage();
+        }
+
+        assertEquals(expectedMessage, realMessage);
+    }
+
+    @Test
+    public void updateQuestion_whenPassInvalidQuizId() {
+        String nombreCursoTest = "unknownCurso";
+        String nombreTemaTest = "unknownTema";
+        String expectedMessage = QuestionConstant.QUESTION_NOT_FOUND;
+        String realMessage = "";
+        try {
+
+            if (this.cursoService.findCursoByNombre(nombreCursoTest) == null)
+                this.cursoService.addNewCurso(nombreCursoTest, "1");
+
+            if (this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest) == null)
+                this.temaService.addNewTema(nombreCursoTest, nombreTemaTest, "", "1");
+
+            QuizDto quizDto = new QuizDto();
+            quizDto.setTitle("nuevo");
+            quizDto.setDescription("examen1");
+            quizDto.setIdTema(this.temaService.findTemaByTitulo(nombreCursoTest, nombreTemaTest).getIdTema().toString()); //este idTema no existe
+
+
+            QuestionDto questionDto = new QuestionDto();
+            questionDto.setIdQuiz(this.quizService.registerNewQuiz(quizDto).getIdQuiz());
+            questionDto.setContent("¿Cuánto es 2 - 2?");
+            questionDto.setOption1("1");
+            questionDto.setOption2("3");
+            questionDto.setOption3("0");
+            questionDto.setOption4("1");
+            questionDto.setAnswer("");
+            questionDto.setIdQuestion("999999999");
+            this.questionService.updateQuestion(questionDto);
+        } catch (UtilityException | CursoNotFoundException | CursoExistsException | TemaNotFoundException | IOException | NotAnImageFileException | TemaExistsException e) {
+            realMessage = e.getMessage();
+        }
+        try {
+            this.cursoService.deleteCurso(nombreCursoTest);
+        } catch (UtilityException e) {
+            e.getMessage();
+        }
+        assertEquals(expectedMessage, realMessage);
+    }
+
+
+
 
 }
